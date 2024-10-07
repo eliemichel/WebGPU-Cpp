641e2ea8d4fcbb453c4ff6173ec9a7002bf511c2
Andre Popovitch <andre@popovit.ch>
2024-10-07 05:33:01 -0500

    Make m_raw const and add a comment explaining why

--- dawn/webgpu.hpp
+++ dawn/webgpu.hpp
@@ -128,7 +128,7 @@ public: \
 	typedef WGPU ## Type W; /* W == WGPU Type */ \
 	constexpr Type(const W& w) : m_raw(w) {} \
 	constexpr operator W() const { return m_raw; } \
-	W m_raw;
+	const W m_raw; /* Ideally, this would be private, but then types generated with this macro would not be structural. */
 
 #define ENUM_ENTRY(Name, Value) \
 	static constexpr W Name = (W)Value;
